733809f06d0af1912a6060953c33a835875489d6
tagodong <tagodong@163.com>
2023-10-19 15:54:57 +0800

    new version for caltrace extract rect

--- Dual-Color-Image-Processing/script/regist2atlas.sh
+++ Dual-Color-Image-Processing/script/regist2atlas.sh
@@ -13,31 +13,29 @@ file_name=(${file_name//,/ });
 for ((i=1;i<${#file_name[*]};i=i+1))
 do
 
-    if [ $i -gt 1 ]; then
-        regist_out_path=$file_dir/${file_name[$i]}/regist2atlas
-        mkdir $regist_out_path
-        if [ $red_flag -eq 1 ];then
-            mean_template=$file_dir/${file_name[$i]}/r/template/mean_template.nii
-        else
-            mean_template=$file_dir/${file_name[$i]}/g/template/mean_template.nii
-        fi
-
-        cd /home/user/tgd/Dual-Color-Image-Processing/Registration/script
-        matlab -nodesktop -nosplash -r "input = '${mean_template}'; output = '${mean_template}'; myunshort; quit"
-
-        ##### Run registration for mean-template to atlas.
-        start_time=$(date +%s)
-
-        antsRegistration -d 3 --float 1 -o [${regist_out_path}/mean2atlas_,${regist_out_path}/mean2atlas_warped.nii.gz] -n WelchWindowedSinc \
-        -u 0 -r [$zbb_atlas,$mean_template,1] -t Rigid[0.1] -m MI[$zbb_atlas,$mean_template,1,32,Regular,0.25] \
-        -c [200x200x0,1e-8,10] -f 8x4x2 -s 3x2x1vox -t Affine[0.1] -m MI[$zbb_atlas,$mean_template,1,32,Regular,0.25] \
-        -c [200x200x0,1e-8,10] -f 8x4x2 -s 3x2x1vox -t SyN[0.05,6,0.4] -m CC[$zbb_atlas,$mean_template,1,2] -c [200x200x200x10,1e-7,10] \
-        -f 8x4x2x1 -s 3x2x1x0vox
-
-        end_time=$(date +%s)
-        cost_time=$[ $end_time-$start_time ]
-        echo "Transform was estimated and the time is $(($cost_time/60))min $(($cost_time%60))s"
+    regist_out_path=$file_dir/${file_name[$i]}/regist2atlas
+    mkdir $regist_out_path
+    if [ $red_flag -eq 1 ];then
+        mean_template=$file_dir/${file_name[$i]}/r/template/mean_template.nii
+    else
+        mean_template=$file_dir/${file_name[$i]}/g/template/mean_template.nii
     fi
+
+    cd /home/user/tgd/Dual-Color-Image-Processing/Registration/script
+    matlab -nodesktop -nosplash -r "input = '${mean_template}'; output = '${mean_template}'; myunshort; quit"
+
+    ##### Run registration for mean-template to atlas.
+    start_time=$(date +%s)
+
+    antsRegistration -d 3 --float 1 -o [${regist_out_path}/mean2atlas_,${regist_out_path}/mean2atlas_warped.nii.gz] -n WelchWindowedSinc \
+    -u 0 -r [$zbb_atlas,$mean_template,1] -t Rigid[0.1] -m MI[$zbb_atlas,$mean_template,1,32,Regular,0.25] \
+    -c [200x200x0,1e-8,10] -f 8x4x2 -s 3x2x1vox -t Affine[0.1] -m MI[$zbb_atlas,$mean_template,1,32,Regular,0.25] \
+    -c [200x200x0,1e-8,10] -f 8x4x2 -s 3x2x1vox -t SyN[0.05,6,0.5] -m CC[$zbb_atlas,$mean_template,1,2] -c [200x200x200x10,1e-7,10] \
+    -f 8x4x2x1 -s 3x2x1x0vox
+
+    end_time=$(date +%s)
+    cost_time=$[ $end_time-$start_time ]
+    echo "Transform was estimated and the time is $(($cost_time/60))min $(($cost_time%60))s"
 
     ##### Apply the transformation to all frames.
     path_g=$file_dir/${file_name[$i]}/g/regist_green/green_demons
@@ -57,11 +55,9 @@ do
 
         start_time=$(date +%s)
 
-        antsApplyTransforms -d 3 -v 0 --float 1 -n WelchWindowedSinc -i $path_g/nii/demons_green_3_$j.nii -r $zbb_atlas -o $path_ants_g/ants_g_$j.nii \
-        -t ${regist_out_path}/mean2atlas_1Warp.nii.gz -t ${regist_out_path}/mean2atlas_0GenericAffine.mat
+        antsApplyTransforms -d 3 -v 0 --float 1 -n WelchWindowedSinc -i $path_g/nii/demons_green_3_$j.nii -r $zbb_atlas -o $path_ants_g/ants_g_$j.nii -t $regist_out_path/mean2atlas_1Warp.nii.gz -t $regist_out_path/mean2atlas_0GenericAffine.mat
 
-        antsApplyTransforms -d 3 -v 0 --float 1 -n WelchWindowedSinc -i $path_r/nii/demons_red_3_$j.nii -r $zbb_atlas -o $path_ants_r/ants_r_$j.nii \
-        -t ${regist_out_path}/mean2atlas_1Warp.nii.gz -t ${regist_out_path}/mean2atlas_0GenericAffine.mat
+        antsApplyTransforms -d 3 -v 0 --float 1 -n WelchWindowedSinc -i $path_r/nii/demons_red_3_$j.nii -r $zbb_atlas -o $path_ants_r/ants_r_$j.nii -t $regist_out_path/mean2atlas_1Warp.nii.gz -t $regist_out_path/mean2atlas_0GenericAffine.mat
 
         end_time=$(date +%s)
         cost_time=$[ $end_time-$start_time ]
